599b2073eea7c14fe14200c3268e5c411db3de54
jong yoon <dhorlawhddbs@naver.com>
2019-11-20 17:50:57 +0900

    test

--- client/src/App/App.test.jsx
+++ client/src/App/App.test.jsx
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
 	ReactDOM.render(<App />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should just fail', () => {
+	expect(false).toBeTruthy();
+});
