a8670ef6dfb9f45fa433f1d1ef2dc8460b5f679f
jong yoon <dhorlawhddbs@naver.com>
2019-11-20 18:03:48 +0900

    test

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Header from '../components/Header';
-import Footer from "../components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
