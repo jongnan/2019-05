705b3bb77fb2719f93b75cdbe501552934523529
jong yoon <dhorlawhddbs@naver.com>
2019-11-20 19:19:38 +0900

    test

--- common_job.sh
+++ common_job.sh
@@ -1,13 +1,9 @@
-if [ -z "$TRAVIS_PULL_REQUEST_SLUG" ]; then
-  GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_BRANCH remotes/origin/develop))
+if [ "$TRAVIS_PULL_REQUEST_SLUG" = "$TRAVIS_REPO_SLUG" ]; then
+  GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_PULL_REQUEST_BRANCH remotes/origin/$TRAVIS_BRANCH))
 else
-  if [ "$TRAVIS_PULL_REQUEST_SLUG" = "$TRAVIS_REPO_SLUG" ]; then
-    GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_PULL_REQUEST_BRANCH remotes/origin/develop))
-  else
-    eval $(echo git remote add prBranch https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git)
-    git fetch prBranch develop
-    GIT_DIFF_RESULT=$(echo bash diff_checking.sh remotes/prBranch/develop remotes/origin/develop)
-  fi
+  eval $(echo git remote add prBranch https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git)
+  eval $(echo git fetch prBranch $TRAVIS_BRANCH) 
+  GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/prBranch/$TRAVIS_BRANCH remotes/origin/$TRAVIS_BRANCH))
 fi
 
 echo $GIT_DIFF_RESULT
