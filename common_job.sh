804378d5fa6517bc724acc717b48d3ebd3570cb7
jong yoon <dhorlawhddbs@naver.com>
2019-11-20 17:16:36 +0900

    test

--- common_job.sh
+++ common_job.sh
@@ -2,7 +2,7 @@ if [ -z "$TRAVIS_PULL_REQUEST_SLUG" ]; then
   GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_BRANCH remotes/origin/develop))
 else
   if [ "$TRAVIS_PULL_REQUEST_SLUG" = "$TRAVIS_REPO_SLUG" ]; then
-    GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_REPO_SLUG remotes/origin/develop))
+    GIT_DIFF_RESULT=$(eval $(echo bash diff_checking.sh remotes/origin/$TRAVIS_PULL_REQUEST_BRANCH remotes/origin/develop))
   else
     eval $(echo git remote add prBranch https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git)
     git fetch prBranch develop
@@ -10,4 +10,4 @@ else
   fi
 fi
 
-echo GIT_DIFF_RESULT
+echo $GIT_DIFF_RESULT
